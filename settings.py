95162f5cbec21f66c7f80d9082d3f5f44a3936c6
p-kirb <peterkiom@gmail.com>
2023-03-29 18:56:57 +0200

    added NB classifier - some issues with combining results of categorical and numerical classifiers

---
+++ settings.py
@@ -0,0 +1,17 @@
+labelledDatapath = "../data/existing-customers.xlsx"
+unlabelledDatapath = "../data/potential-customers.xlsx"
+
+redundantFeatures = ['RowID', 'education']
+categoricalFeatures = ['workclass', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'native-country']
+numericalFeatures = ['age', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+
+
+
+'''
+Decision Trees hyperparameters
+'''
+
+
+'''
+Naive Bayes hyperparameters
+'''
